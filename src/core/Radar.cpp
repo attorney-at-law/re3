43c2e711db2f176c35e3c609718057eefff5676f
attorney-at-law <36831829+attorney-at-law@users.noreply.github.com>
2026-05-27 22:31:25 +0800

    Update Radar.cpp
    show hidden packages location

--- src/core/Radar.cpp
+++ src/core/Radar.cpp
@@ -19,6 +19,7 @@
 #include "SaveBuf.h"
 #include "Streaming.h"
 #include "SpecialFX.h"
+#include "Pickups.h"
 
 float CRadar::m_radarRange;
 sRadarTrace CRadar::ms_RadarTrace[NUMRADARBLIPS];
@@ -761,6 +762,29 @@ void CRadar::DrawBlips()
 					break;
 			}
 		}
+
+		for(int slot = NUMPICKUPS - 1; slot >= 0; slot--) 
+			if(CPickups::aPickUps[slot].m_eType == PICKUP_COLLECTABLE1) {
+				TransformRealWorldPointToRadarSpace(in, (CPickups::aPickUps[slot].m_vecPos));
+				float dist = LimitRadarPoint(in);
+				TransformRadarPointToScreenSpace(out, in);
+#ifdef TRIANGULAR_BLIPS
+				const CVector &pos = FindPlayerCentreOfWorld_NoSniperShift();
+				const CVector &blipPos = CPickups::aPickUps[slot].m_vecPos;
+				uint8 mode = BLIP_MODE_TRIANGULAR_UP;
+				if(blipPos.z - pos.z <= 2.0f) {
+					if(blipPos.z - pos.z < -4.0f)
+						mode = BLIP_MODE_TRIANGULAR_DOWN;
+					else
+						mode = BLIP_MODE_SQUARE;
+				}
+				ShowRadarTraceWithHeight(out.x, out.y, 1, 255, 255, 255, 255, mode);
+#else
+				ShowRadarTrace(out.x, out.y, 1, 255, 255, 255, 255);
+#endif
+			}
+
+
 #ifdef MENU_MAP
 		if (CMenuManager::bMenuMapActive) {
 			CVector2D in, out;
